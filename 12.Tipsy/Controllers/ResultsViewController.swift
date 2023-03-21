b9344357b406066eaf041faa236df8b448823051
Alex Asichka <sanechka3@mail.ru>
2023-03-21 16:45:00 +0200

    firstScreen

---
+++ 12.Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  12.Tipsy
+//
+//  Created by Aleksandra Asichka on 2023-03-21.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
