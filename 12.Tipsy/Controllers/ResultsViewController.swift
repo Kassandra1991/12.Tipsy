76d6dd9ee4ab71153d24280dccdf5fa2565f88ff
Alex Asichka <sanechka3@mail.ru>
2023-03-21 16:47:19 +0200

    second screen

--- 12.Tipsy/Controllers/ResultsViewController.swift
+++ 12.Tipsy/Controllers/ResultsViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var settingsLabel: UILabel!
     var result = "0.0"
     var tip = 10
     var split = 2
@@ -17,18 +19,14 @@ class ResultsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self. dismiss(animated: true)
     }
-    */
-
+    
 }
